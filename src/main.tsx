73de0b767d8a79c4dc82c304d7cfcb1196936f12
DRX191 <roussbel.medina@unah.hn>
2026-04-08 14:50:58 -0600

    aggrid bugfix not showing correctly

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
 import App from "./App";
 import "./styles.css";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
